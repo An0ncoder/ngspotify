d835e5b57ba9b32a4e3cb124441b4a5f97c43e04
unknown <sascha.jaenicke@mind-me.de>
2017-04-19 12:52:06 +0200

    Add Navbar and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,28 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes  } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { NavbarTopComponent } from './components/navbar-top/navbar-top.component';
+import { AboutComponent } from './components/about/about.component';
+import { SearchComponent } from './components/search/search.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent, NavbarTopComponent ],
+  imports:      [ 
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '', 
+        component: SearchComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent 
+      }
+    ]) 
+    ],
+  declarations: [ AppComponent, NavbarTopComponent, SearchComponent, AboutComponent  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
